67eb3bc58471c3c9ef5123f96af6195c09b079d4
Paul HELLEU <57185255+paul-helleu@users.noreply.github.com>
2024-07-12 15:52:48 +0200

    feat: LinkedList addToTail method

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -4,13 +4,25 @@ public class LinkedList {
   public LinkedList() {
     this.head = null;
   }
-
+  
   public void addToHead(String data) {
     Node newHead = new Node(data);
     Node currentHead = this.head;
     this.head = newHead;
     if (currentHead != null) {
       this.head.setNextNode(currentHead);
+    }
+  }
+
+  public void addToTail(String data) {
+    Node tail = this.head;
+    if (tail == null) {
+      this.head = new Node(data);
+    } else {
+      while (tail.getNextNode() != null) {
+        tail = tail.getNextNode();
+      }
+        tail.setNextNode(new Node(data));
     }
   }
 }
