50bd3b9c065dbe63bb81c5080909ce5590fb8567
Paul HELLEU <57185255+paul-helleu@users.noreply.github.com>
2024-07-12 15:52:04 +0200

    feat: LinkedList addToHead method

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -4,4 +4,13 @@ public class LinkedList {
   public LinkedList() {
     this.head = null;
   }
+
+  public void addToHead(String data) {
+    Node newHead = new Node(data);
+    Node currentHead = this.head;
+    this.head = newHead;
+    if (currentHead != null) {
+      this.head.setNextNode(currentHead);
+    }
+  }
 }
